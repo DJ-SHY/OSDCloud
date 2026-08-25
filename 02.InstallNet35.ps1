dd2edbc02b0602baba626587343d64429a41eac0
DJ-SHY <90383965+DJ-SHY@users.noreply.github.com>
2026-08-25 15:01:47 +0800

    Update 02.InstallNet35.ps1

--- 02.InstallNet35.ps1
+++ 02.InstallNet35.ps1
@@ -1,15 +1,15 @@
 Write-Host "[OSDCloud] Unlocking Windows Update & Installing .NET 3.5..." -ForegroundColor Cyan
 
-$ServicingKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing"
-if (-not (Test-Path $ServicingKey)) { New-Item -Path $ServicingKey -Force | Out-Null }
-Set-ItemProperty -Path $ServicingKey -Name "LocalSourceConfigForFeatures" -Value 2 -Type DWord -Force
+# $ServicingKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing"
+# if (-not (Test-Path $ServicingKey)) { New-Item -Path $ServicingKey -Force | Out-Null }
+# Set-ItemProperty -Path $ServicingKey -Name "LocalSourceConfigForFeatures" -Value 2 -Type DWord -Force
 
-$WuKey = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
-if (-not (Test-Path $WuKey)) { New-Item -Path $WuKey -Force | Out-Null }
-Set-ItemProperty -Path $WuKey -Name "UseWUServer" -Value 0 -Type DWord -Force
+# $WuKey = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+# if (-not (Test-Path $WuKey)) { New-Item -Path $WuKey -Force | Out-Null }
+# Set-ItemProperty -Path $WuKey -Name "UseWUServer" -Value 0 -Type DWord -Force
 
-Set-Service -Name "wuauserv" -StartupType Automatic -ErrorAction SilentlyContinue
-Restart-Service -Name "wuauserv" -Force -ErrorAction SilentlyContinue
+# Set-Service -Name "wuauserv" -StartupType Automatic -ErrorAction SilentlyContinue
+# Restart-Service -Name "wuauserv" -Force -ErrorAction SilentlyContinue
 
 try {
     Add-WindowsCapability -Online -Name "NetFX3~~~~" -ErrorAction Stop | Out-Null
